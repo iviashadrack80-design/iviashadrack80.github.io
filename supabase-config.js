7b44b2bb3c7c436b46abc99ee515ee3ec2b5401e
iviashadrack80-design <iviashadrack80@gmail.com>
2026-09-26 19:13:21 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // The publishable key is safe to use in a browser when your Supabase
 // Row Level Security (RLS) policies are configured correctly.
 
-const SUPABASE_URL = "https://xdykszqzzjdwcubvuekw.supabase.co";
+const SUPABASE_URL = "https://xdykszgzzjdwcubvuekw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_biEsuAbpTz_M4tqoidCrDg_MPtogUrd";
